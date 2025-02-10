cd2dc8f29d6e929f0198cbc702cd84d7d8027175
ExtendedGuru4883 <152730089+ExtendedGuru4883@users.noreply.github.com>
2025-02-10 12:17:32 +0100

    Rimosso event listener non utilizzato

--- assets/js/event-listeners.js
+++ assets/js/event-listeners.js
@@ -1,8 +1,6 @@
 //--Questo script contiene tutti gli event listener
 
 //--Sul form di registrazione
- //Ascolta per input nel campo password -- NON USATO AL MOMENTO
- //document.signUpForm.password.addEventListener("input", validatePassword);
  //Ascolta per l'evento di submit del form e di default passa event come primo argomento al checkFormInputs
  document.signUpForm.addEventListener("submit", checkFormInputs);
  //Ascolta per l'evento di click sul link alla privacy policy
