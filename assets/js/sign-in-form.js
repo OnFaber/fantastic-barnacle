911a4a35d8556d820da678613fd79a25789274e4
ExtendedGuru4883 <152730089+ExtendedGuru4883@users.noreply.github.com>
2025-02-10 18:52:29 +0100

    Aggiunta logica al form di log in

---
+++ assets/js/sign-in-form.js
@@ -0,0 +1,51 @@
+//--Script relativi al form di accesso
+
+//--Event listener
+//Ascolta per l'evento di submit del form e di default passa event come primo argomento al checkFormInputs
+document.signInForm.addEventListener("submit", checkFormInputs);
+
+//--Funzioni
+//Funzione che ascolta l'evento click del bottone submit
+function checkFormInputs (event) { //event è passato di default come primo argomento dall'event listener
+    const form = document.signInForm;
+    const emailField = form.email, email = emailField.value; 
+    const passwordField = form.password, password = passwordField.value;
+    
+    //Validazione password
+    if(!validatePassword(password)) {
+        passwordField.classList.add("error");
+        passwordField.focus();
+        event.preventDefault();
+    } else {
+        passwordField.classList.remove("error");
+    }
+    //Validazione email
+    if(!validateEmail(email)) {
+        emailField.classList.add("error");
+        emailField.focus();
+        event.preventDefault();
+    } else {
+        emailField.classList.remove("error");
+    }
+}
+
+//Funzione che controlla se la password rispetta i requisiti stabiliti
+function validatePassword (password) {
+    const isPasswordEmpty = (password == "");
+
+    console.log("Password vuota:"+isPasswordEmpty) //Debug
+    return (!isPasswordEmpty);
+    
+}
+
+//Funzione che controlla se l'email rispetta i requisiti stabiliti
+function validateEmail (email) {
+    const emailPattern = /[^@.+]+@[a-zA-Z]+\.[a-zA-Z]+/ //Formato di una mail valida
+    
+    if (!emailPattern.test(email)) { //Controllo che il formato sia corretto
+        console.log("Formato mail non valido") //Debug
+        return false;
+    } else {
+        return true;
+    }
+}
