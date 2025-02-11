2d3709538a397df2f27c05045930bfa634bfbe4e
Fabrizio <fabrizio.boezi@outlook.it>
2025-02-11 02:42:26 +0100

    proposta per formattazione validazione form

---
+++ assets/js/ErrorHandler.js
@@ -0,0 +1,25 @@
+class ErrorHandler {
+  static showError(inputField, message, duration = 2000) {
+    this.clearError(inputField);
+    const errorMessage = document.createElement("span");
+    errorMessage.classList.add("error-message");
+    errorMessage.textContent = message;
+    inputField.classList.add("error");
+    inputField.insertAdjacentElement("afterend", errorMessage);
+
+    inputField.focus();
+
+    setTimeout(() => {
+      this.clearError(inputField);
+    }, duration);
+  }
+
+  static clearError(inputField) {
+    inputField.classList.remove("error");
+    const existingError =
+      inputField.parentElement.querySelector(".error-message");
+    if (existingError) existingError.remove();
+  }
+}
+
+export default ErrorHandler;
