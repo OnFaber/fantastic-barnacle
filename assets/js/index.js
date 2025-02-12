2093c60d7e37109dc5b9675b147a9aa11af5d448
Fabrizio <fabrizio.boezi@outlook.it>
2025-02-12 15:19:52 +0100

    aggiornata homepage, con le grid area

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,21 @@
-const user = window.localStorage.getItem("user");
+const button = document.getElementsByClassName("button")[0];
+const nav = document.getElementsByTagName("nav")[0];
+const main = document.getElementsByTagName("main")[0];
+const body = document.getElementsByTagName("body")[0];
 
-user ? null : (window.location.href = "/sign-up.html");
+function hide() {
+  nav.style.display = "none";
+}
+
+function show() {
+  nav.style.display = "flex";
+}
+
+button.addEventListener("click", function (e) {
+  body.classList.toggle("body");
+  if (nav.style.display == "none") {
+    nav.style.display = "flex";
+  } else {
+    nav.style.display = "none";
+  }
+});
