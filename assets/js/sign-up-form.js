553c9dedacd02b69c04266b3e7d5309af6ca5a35
ExtendedGuru4883 <152730089+ExtendedGuru4883@users.noreply.github.com>
2025-02-12 11:19:36 +0100

    Rimosso commento non più pertinente

--- assets/js/sign-up-form.js
+++ assets/js/sign-up-form.js
@@ -3,20 +3,7 @@ import Validators from "./Validators.js";
 import { SignUpForm } from "./SignUpForm.js";
 
 const signUpForm = new SignUpForm(document.signUpForm);
-/**
- * Funzione che gestisce l'evento di submit del form di registrazione.
- * Esegue la validazione dei campi di input (password, email e privacy policy) e
- * mostra i messaggi di errore appropriati utilizzando la classe `ErrorHandler`.
- * Se uno dei campi non è valido, impedisce l'invio del form.
- *
- * @param {Event} event - L'evento di submit del form. È passato di default dal listener dell'evento.
- *
- * @returns {void} Non restituisce nulla, ma previene l'invio del form in caso di errori di validazione.
- *
- * @see {@link Validators} per la validazione della password e dell'email.
- * @see {@link ErrorHandler} per la gestione della visualizzazione degli errori.
- *
- */
+
 function checkFormInputs(event) {
   event.preventDefault();
   let isValid = true;
