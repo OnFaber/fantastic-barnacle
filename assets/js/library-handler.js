84e1a9a9c873372882706633a2398c065fa0e817
ExtendedGuru4883 <152730089+ExtendedGuru4883@users.noreply.github.com>
2025-02-14 11:13:33 +0100

    Eliminato script inutilizzato. Iniziato script gestione libreria

---
+++ assets/js/library-handler.js
@@ -0,0 +1,7 @@
+//--Event listener
+document.getElementById("addBookButton").addEventListener("click", addBook);
+
+//--Funzioni legate a event listener
+function addBook () {
+    console.log("ciao");
+}
