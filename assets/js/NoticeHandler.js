1699e1e7d626d86d0183d20f8e95df20766977a4
ExtendedGuru4883 <152730089+ExtendedGuru4883@users.noreply.github.com>
2025-02-15 23:52:52 +0100

    Riorganizzato codice NoticeHandler

--- assets/js/NoticeHandler.js
+++ assets/js/NoticeHandler.js
@@ -3,85 +3,59 @@ class NoticeHandler {
   
   //Error
   static showError(inputField, message, duration, fixed = false) { //Se passato duration=0 il messaggio è permanente
-    
-    // Cancella il timeout attivo, se presente
+    this.#showNotice(inputField, message, duration, fixed, "error");
+  }
+  
+  //Success
+  static showSuccess(inputField, message, duration, fixed = false) { //Se passato duration=0 il messaggio è permanente
+    this.#showNotice(inputField, message, duration, fixed, "success");
+  }
+  
+  //Message
+  static showMessage(inputField, message, duration, fixed = false) { //Se passato duration=0 il messaggio è permanente
+    this.#showNotice(inputField, message, duration, fixed, "message");
+  }
+  
+  //--Condivise
+  //Funzione generica per mostrare un messaggio
+  static #showNotice (inputField, message, duration, fixed, messageType) {
+    //Se c'è un timeout attivo lo cancello
+    //Per evitare che il timeout di un'eventuale chiamata
+    //Precedente agisca cancellando i messaggi scritti dalla chiamata attuale
     if (this.activeTimeout) {
       clearTimeout(this.activeTimeout);
       this.activeTimeout = null;
     }
-    
+
     const notice = document.querySelector(".notice");
-    this.clearClasses(notice, "error", fixed);
-    if (inputField != null) this.clearMessage(inputField, "error");
-    notice.classList.add("error");
+    this.#clearClasses(notice, messageType, fixed);
+    if (inputField != null) this.#clearMessage(inputField, messageType);
+    notice.classList.add(messageType);
     notice.classList.add("fade_in");
     if (fixed) notice.classList.add("fixed");
     notice.textContent = message;
-    if (inputField != null) inputField.classList.add("error");
-    
-    if (inputField != null) inputField.focus();
-    
-    if (duration != 0) { //Con duration=0 indico un messaggio permanente
-      this.activeTimeout = setTimeout(() => {
-        this.clearClasses (notice, "error", fixed);
-        if (inputField != null) this.clearMessage(inputField, "error");
-      }, duration);
-    }
-  }
-  
-  //Success
-  static showSuccess(inputField, message, duration, fixed = false) {
-    this.clearClasses(notice, "success", fixed);
-    if (inputField != null) this.clearMessage(inputField, "success");
-    const notice = document.querySelector(".notice");
-    notice.classList.add("success");
-    notice.classList.add("fade_in");
-    if (fixed) notice.classList.add("fixed");
-    notice.textContent = message;
-    if (inputField != null) inputField.classList.add("success");
+    if (inputField != null) inputField.classList.add(messageType);
     // inputField.insertAdjacentElement("afterend", notice);
     
     if (inputField != null) inputField.focus();
     
     if (duration != 0) { //Con duration=0 indico un messaggio permanente
       setTimeout(() => {
-        this.clearClasses(notice, "success", fixed);;
-        if (inputField != null) this.clearMessage(inputField, "success");
+        this.#clearClasses(notice, messageType, fixed);;
+        if (inputField != null) this.clearMessage(inputField, messageType);
       }, duration);
     }
   }
-  
-  //Message
-  static showMessage(inputField, message, duration, fixed = false) {
-    this.clearClasses(notice, "message", fixed);
-    if (inputField != null) this.clearMessage(inputField, "message");
-    const notice = document.querySelector(".notice");
-    notice.classList.add("message");
-    notice.classList.add("fade_in");
-    if (fixed) notice.classList.add("fixed");
-    notice.textContent = message;
-    if (inputField != null) inputField.classList.add("message");
-    // inputField.insertAdjacentElement("afterend", notice);
-    
-    if (inputField != null) inputField.focus();
-    
-    if (duration != 0) { //Con duration=0 indico un messaggio permanente
-      setTimeout(() => {
-        this.clearClasses(notice, "message", fixed);;
-        if (inputField != null) this.clearMessage(inputField, "message");
-      }, duration);
-    }
-  }
-  
-  //Condivise
-  static clearMessage(inputField, message) {
-    inputField.classList.remove(message);
+
+  //Funzioni per pulire prima di una nuova esecuzione o dopo il timeout
+  static #clearMessage(inputField, messageType) {
+    inputField.classList.remove(messageType);
     const existingMessage =
-    inputField.parentElement.querySelector(`.${message}-message`);
+    inputField.parentElement.querySelector(`.${messageType}-message`);
     if (existingMessage) existingMessage.remove();
   }
   
-  static clearClasses (notice, messageType, fixed) {
+  static #clearClasses (notice, messageType, fixed) {
     notice.classList.remove("fade_in");
     notice.classList.remove(messageType);
     if (fixed) notice.classList.remove("fixed");
