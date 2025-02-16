5f9226cf439a7495966cdb517913be7d5f1773c4
ExtendedGuru4883 <152730089+ExtendedGuru4883@users.noreply.github.com>
2025-02-16 14:28:21 +0100

    Aggiunti link alle librerie degli utenti visualizzati in homepage

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,6 +19,24 @@ closeBtn.addEventListener("click", () => {
 });
 
 //--Funzioni eseguite subito
+//-Decido cosa mostrare in base allo stato di login
+//Controllo se l'utente è loggato e con che account
+const loggedInUser = AccountHandler.whoIsLoggedIn();
+let user = null;
+if (loggedInUser != null) { //Se è loggato
+  //Carico i suoi dati dal local storage
+  user = new User();
+  user = JSON.parse(localStorage.getItem(`user+${loggedInUser}`));
+  //Inserisco il link al suo account
+  document.getElementById("homepageYourAccountHref").innerHTML = "<a href='./account.html'>Your account<a>";
+} else { //Se non è loggato
+  //Nascondo il link alla propria libreria
+  document.getElementById("indexYourLibraryListItem").classList.add("hidden");
+  //Inserisco il link alla pagina di sign in
+  document.getElementById("homepageYourAccountHref").innerHTML = "<a href='./sign-in.html'>Sign in<a>";
+}
+
+//-Mostro la lista degli utenti o l'avviso che non ce ne sono
 //Prendo l'elemento main della homepage
 let indexMain = document.getElementById("indexMain")
 //Carico la lista di tutti gli utenti registrati
@@ -37,23 +55,19 @@ if (registeredUsers.length == 0) { //--Se non sono utenti registrati
   const usersList = HTMLGenerator.generateLastChild(header, "ul", `Top ${Math.min(10, registeredUsers.length)} users:`);
   //Inserisco nella lista i primi 10
   for (let i=0; i<Math.min(10, registeredUsers.length); i++) {
-    const userDisplayedInfo = `${registeredUsers[i].credentials.username} - ${registeredUsers[i].library.length} books`;
-    console.log (userDisplayedInfo);
+    //Carico username e numero di libri in libreria di ogni utente
+    let thisUserUsername = registeredUsers[i].credentials.username;
+    const thisUserLibraryLength = registeredUsers[i].library.length
+    let thisUserLibraryHref = `./library.html?user=${thisUserUsername}`;
+    if (user != null) { //Se c'è un utente loggato
+      if (user.credentials.username == thisUserUsername) { //Controllo se è questo utente
+        thisUserUsername += " (you)"; //Se lo è appendo "(you)" all'username che mostrerò
+        thisUserLibraryHref = `./library.html?user=me`; //E modifico il link per indirizzare alla sua libreria
+      }
+    }
+    //Costruisco le informazioni da mostrare sull'utente 
+    const userDisplayedInfo = `<a href="${thisUserLibraryHref}">${thisUserUsername}</a> - ${thisUserLibraryLength} books`;
+    //Genero la list entry con le informazioni sull'utente
     HTMLGenerator.generateLastChild(usersList, "li", userDisplayedInfo);
   }
-}
-
-//Controllo se l'utente è loggato e con che account
-const loggedInUser = AccountHandler.whoIsLoggedIn();
-if (loggedInUser != null) { //Se è loggato
-  //Carico i suoi dati dal local storage
-  var user = new User();
-  user = JSON.parse(localStorage.getItem(`user+${loggedInUser}`));
-  //Inserisco il link al suo account
-  document.getElementById("homepageYourAccountHref").innerHTML = "<a href='./account.html'>Your account<a>";
-} else { //Se non è loggato
-  //Nascondo il link alla propria libreria
-  document.getElementById("indexYourLibraryListItem").classList.add("hidden");
-  //Inserisco il link alla pagina di sign in
-  document.getElementById("homepageYourAccountHref").innerHTML = "<a href='./sign-in.html'>Sign in<a>";
 }
